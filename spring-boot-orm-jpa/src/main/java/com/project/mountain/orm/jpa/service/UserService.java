569e7412a93ba1cdc40dacd29fdeeafc59f0e960
kejiefu <12345678>
2020-09-19 12:40:03 +0800

    提交orm jpa

---
+++ spring-boot-orm-jpa/src/main/java/com/project/mountain/orm/jpa/service/UserService.java
@@ -0,0 +1,10 @@
+package com.project.mountain.orm.jpa.service;
+
+/**
+ * @author kejiefu
+ * @Classname
+ * @Description
+ * @Date 2020/9/19 12:32
+ */
+public interface UserService {
+}
---
+++ spring-boot-orm-jpa/src/main/java/com/project/mountain/orm/jpa/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.project.mountain.orm.jpa.service.impl;
+
+import com.project.mountain.orm.jpa.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author kejiefu
+ * @Classname
+ * @Description
+ * @Date 2020/9/19 12:33
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+
+
+}
