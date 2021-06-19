fb093c6d57e851801feba2d1a2668ae3284606d5
kejiefu <kejiefu@126.com>
2021-06-19 13:40:28 +0800

    测试

--- spring-boot-dynamic-sharding-jdbc/src/test/java/com/mountain/dynamic/service/impl/TeacherServiceImplTest.java
+++ spring-boot-dynamic-sharding-jdbc/src/test/java/com/mountain/dynamic/service/impl/TeacherServiceImplTest.java
@@ -1,7 +1,11 @@
 package com.mountain.dynamic.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.mountain.dynamic.ApplicationTests;
+import com.mountain.dynamic.entity.Teacher;
+import com.mountain.dynamic.entity.User;
 import com.mountain.dynamic.service.TeacherService;
+import com.mountain.dynamic.service.UserService;
 import org.junit.jupiter.api.Test;
 
 import javax.annotation.Resource;
@@ -17,10 +21,20 @@ public class TeacherServiceImplTest extends ApplicationTests {
     @Resource
     TeacherService teacherService;
 
+    @Resource
+    UserService userService;
+
     @Test
     public void test1() throws Exception {
         teacherService.saveTeacher();
     }
 
+    @Test
+    public void test2() throws Exception {
+        User user = userService.getById(1405861484948357120L);
+        System.out.println(JSONObject.toJSONString(user));
+        Teacher teacher = teacherService.getById(1405863748039606272L);
+        System.out.println(JSONObject.toJSONString(teacher));
+    }
 
 }
