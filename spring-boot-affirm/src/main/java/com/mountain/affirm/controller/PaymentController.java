40bac7075ff5cac95c893a12210eaf90dfb481a2
kejiefu <kejiefu@126.com>
2022-08-15 16:57:47 +0800

    affirm支付修改

--- spring-boot-affirm/src/main/java/com/mountain/affirm/controller/PaymentController.java
+++ spring-boot-affirm/src/main/java/com/mountain/affirm/controller/PaymentController.java
@@ -1,10 +1,14 @@
 package com.mountain.affirm.controller;
 
+import com.mountain.affirm.service.AffirmService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
 
 
 /**
@@ -18,7 +22,12 @@ public class PaymentController {
 
     public static final String PAYPAL_SUCCESS_URL = "pay/success";
 
+    public static final String PAYPAL_CHECK_URL = "pay/check";
+
     private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Resource
+    private AffirmService affirmService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String index() {
@@ -26,18 +35,26 @@ public class PaymentController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = PAYPAL_SUCCESS_URL)
-    public String successPay() {
+    public String successPay(@RequestParam("checkout_token") String checkoutToken) {
+        log.info("successPay...");
         try {
+            affirmService.checkPay(checkoutToken);
             return "success";
         } catch (Exception ex) {
-            log.error(ex.getMessage());
+            log.error("successPay:", ex);
         }
         return "redirect:/";
     }
 
     @RequestMapping(method = RequestMethod.GET, value = PAYPAL_CANCEL_URL)
     public String cancelPay() {
+        log.info("cancelPay...");
         return "cancel";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = PAYPAL_CHECK_URL)
+    public String checkPay(@RequestParam("checkout_token") String checkoutToken) {
+        return affirmService.checkPay(checkoutToken);
     }
 
 
--- spring-boot-affirm/src/main/java/com/mountain/affirm/service/AffirmService.java
+++ spring-boot-affirm/src/main/java/com/mountain/affirm/service/AffirmService.java
@@ -3,10 +3,16 @@ package com.mountain.affirm.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+/**
+ * https://docs.affirm.com/developers/reference/list_transactions
+ */
 @Service
 @Slf4j
 public class AffirmService {
 
-
+    public String checkPay(String checkoutToken) {
+        log.info("checkPay:{}", checkoutToken);
+        return "";
+    }
 
 }
