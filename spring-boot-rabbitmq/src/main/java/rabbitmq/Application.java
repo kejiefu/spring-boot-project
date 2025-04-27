9066880c01cd565c03a312e16ad593d4e56c1938
kejiefu <kejiefu@126.com>
2025-04-27 16:00:35 +0800

    rabbitmq

---
+++ spring-boot-rabbitmq/src/main/java/rabbitmq/Application.java
@@ -0,0 +1,27 @@
+package rabbitmq;
+
+import cn.hutool.extra.spring.EnableSpringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kejiefu
+ * @Classname
+ * @Description
+ * @Date 2020/9/19 11:18
+ */
+@SpringBootApplication
+@EnableSpringUtil
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("~~~~~~~~~~~~~~~~ program start~~~~~~~~~~~~~~~~!");
+        SpringApplication.run(Application.class, args);
+        logger.info("~~~~~~~~~~~~~~~~ program execute successfully~~~~~~~~~~~~~~~~!");
+    }
+
+}
